ed2e3626df91b277b5e5fdf9b09c7070ba8557e5
Khang Nguyen <khang@folafi.io>
2023-03-30 21:08:11 -0400

    Updated

--- nvim/lua/khang/packer.lua
+++ nvim/lua/khang/packer.lua
@@ -52,7 +52,10 @@ return require('packer').startup(function(use)
             {'L3MON4D3/LuaSnip'},     -- Required
             }
         }
-        
+
+    use 'preservim/vimux'        
+    use { "catppuccin/nvim", as = "catppuccin" }   
+    
     if packer_bootstrap then
         require('packer').sync()
     end
